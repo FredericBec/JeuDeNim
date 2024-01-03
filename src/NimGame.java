b2cced24f13401583f7494d76685f043b789e02a
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-03 15:05:34 +0100

    ajout gestion des allumettes et boucle du jeu

--- src/NimGame.java
+++ src/NimGame.java
@@ -4,15 +4,9 @@ import java.util.Scanner;
 public class NimGame {
 
 	public static Scanner scan = new Scanner(System.in);
+	public static int matchesCount = 21;
 	
 	public static void main(String[] args) {
-		
-		
-		firstPlayer();
-		
-	}
-	
-	public static void firstPlayer() {
 		String playerOne, playerTwo;
 		
 		System.out.println("Joueur 1, quel est votre nom?");
@@ -21,10 +15,44 @@ public class NimGame {
 		System.out.println("Joueur 2, quel est votre nom?");
 		playerTwo = scan.nextLine();
 		
+		String currentPlayer = firstPlayer(playerOne, playerTwo);
+		System.out.println(currentPlayer);
+		
+		while(matchesCount > 0) {
+			System.out.println("Allumette restantes :" + matchesCount);
+			System.out.println("Joueur " + currentPlayer + ", choisissez un nombre d'allumette");
+			int matchesNumber = scan.nextInt();
+			
+			if(validMove(matchesNumber)) {
+				removeMatches(matchesNumber);
+				currentPlayer = currentPlayer.equals(playerOne) ? playerTwo : playerOne;
+			}else System.out.println("Nombre d'allumettes non valide !!");
+		}
+		System.out.println("le joueur " + currentPlayer + " a pris la dernière allumette");
+		
+		
+		
+	}
+	
+	public static String firstPlayer(String playerOne, String playerTwo) {
+		
+		
 		String[] playerList = {playerOne, playerTwo};
 		
 		int alea = new Random().nextInt(2);
-		System.out.println(playerList[alea]);
+		System.out.println("Le joueur qui commence est : ");
+		return playerList[alea];
+	}
+	
+	public static int removeMatches(int matches) {
+		
+		return matchesCount -= matches;
+	}
+	
+	public static boolean validMove(int matches) {
+		
+		return matches >= 1 && matches <= 4 && matches <= matchesCount;
+			
 	}
 
 }
