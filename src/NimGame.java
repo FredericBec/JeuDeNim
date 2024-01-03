6d8a22ff83ade0bea26f584f76c8fccbe4564cc8
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-03 11:48:18 +0100

    choix du premier joueur

--- src/NimGame.java
+++ src/NimGame.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
 
 public class NimGame {
 
+	public static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
-
+		
+		playerChoice();
+		
+	}
+	
+	public static void playerChoice() {
+		String playerOne, playerTwo;
+		
+		System.out.println("Joueur 1, quel est votre nom?");
+		playerOne = scan.nextLine();
+		
+		System.out.println("Joueur 2, quel est votre nom?");
+		playerTwo = scan.nextLine();
+		
+		String[] playerList = {playerOne, playerTwo};
+		
+		int alea = new Random().nextInt(2);
+		System.out.println(playerList[alea]);
 	}
 
 }
